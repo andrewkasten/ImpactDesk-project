0dcce089ef14b0cb8f8007591707bc31cb6c0ff7
and3k <andrewjkasten@gmail.com>
2026-04-08 15:57:42 -0600

    needed auth in default conf

--- run-compose-prod.sh
+++ run-compose-prod.sh
@@ -4,5 +4,6 @@ docker compose -f docker-compose.prod.yml build --no-cache
 docker compose -f docker-compose.prod.yml up -d db
 
 sleep 3
+docker compose -f docker-compose.prod.yml run --rm api python manage.py makemigrations --noinput
 docker compose -f docker-compose.prod.yml run --rm api python manage.py migrate --noinput
 docker compose -f docker-compose.prod.yml up -d
