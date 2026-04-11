c0724f2a1609a30138dce11514dd1d6b803b8b4a
and3k <andrewjkasten@gmail.com>
2026-04-11 07:23:59 -0600

    corrected container name

--- run-compose-prod.sh
+++ run-compose-prod.sh
@@ -2,7 +2,7 @@ docker compose -f docker-compose.prod.yml build --no-cache
 docker compose -f docker-compose.prod.yml up -d
 
 sleep 5
-docker exec impactdesk-project-copy-api-1 python manage.py migrate
-docker exec impactdesk-project-copy-api-1 python manage.py loaddata initial_data
+docker exec impactdesk-project-api-1 python manage.py migrate
+docker exec impactdesk-project-api-1 python manage.py loaddata initial_data
 
 
